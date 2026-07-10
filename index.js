8d6c00d1a8816549aaa48c77fa03d3a9715bdba6
omar44s <lymr90280@gmail.com>
2026-07-10 17:33:19 +0300

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
